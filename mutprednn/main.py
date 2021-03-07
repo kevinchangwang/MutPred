5cb75de8cecbb13d8a7d7eaf201b2f7470f71db7
kevinchangwang <kevin4@ualberta.ca>
2021-03-07 02:18:25 -0500

    Moving most code into Classes or Functions

--- mutprednn/main.py
+++ mutprednn/main.py
@@ -6,14 +6,47 @@ import os
 import requests
 from tqdm.auto import tqdm
 from mutprednn import sequence_mut
+from mutprednn import input_params
 
 
-tokenizer = BertTokenizer.from_pretrained("/home/kevinchangwang/RothLab/MutPred/mutprednn/ProtTrans-BFD/", do_lower_case = False)
-model = BertModel.from_pretrained("/home/kevinchangwang/RothLab/MutPred/mutprednn/ProtTrans-BFD/")
-input_sequence = #get input sequence from file
-fe = pipeline("feature-extraction", model = model, tokenizer = tokenizer, device = 0)
-protein_sequence = sequence_mut(input_sequence)
-embedding = np.array(fe(protein_sequence))
+class get_prot_seq_embedding:
+    def __init__(self, model_dir, prot_seq):
+        self.model_dir = model_dir
+        self.prot_seq = prot_seq
+        self.tokenizer = BertTokenizer.from_pretrained(model_dir, do_lower_case = False)
+        self.model = BertModel.from_pretrained(model_dir)
+    def generate_embedding(self):
+        fe = pipeline("feature-extraction", model = self.model, tokenizer = self.tokenizer)
+        embedding = fe(self.prot_seq)
+        return np.array(embedding)
 
-print(embedding.shape)
+class get_euclidean_distance:
+    def __init__(self,wt_embed, mut_embed):
+        self.wt_embed = wt_embed
+        self.mut_embed = mut_embed
+    def calc_distance(self):
+        dist = np.linalg.norm(self.wt_embed, self.mut_embed)
+        return dist
 
+class seq_mut:
+    def __init__(self, input_seq, position, mut_res):
+        self.input_seq = input_seq
+        self.position = position
+        self.mut_res = mut_res
+    def mutant(self):
+        self.input_seq[position] = self.mut_res
+        return self.input_seq
+
+class parse_input_file:
+    def __init__(self, input_file_path):
+        self.input_file_path = input_file_path
+    def read_input(self):
+        pass ### YAML input file reading here 
+
+
+
+def main():
+    pass
+
+if __name__ =="__main__":
+    main()
